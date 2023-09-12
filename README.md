7d461b96540219fd0bff362e30a783f8eaff5ee5
Dan Murphy <murphy.dan.s@gmail.com>
2023-09-12 00:16:32 -0400

    finished simulation

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
 # BingoSim
+
+Simulating bingo games with a varying number of cards to learn how soon you should expect a winner.  
+
+## Example output: 
+
+```
+
+-----BINGO SIMULATION ANALYSIS------
+
+
+
+Finding how soon games finish as more cards are added.
+
+Running 1000 simulations for different numbers of cards up to 30.
+
+
+Finished 1000 simulations for 1 card
+Finished 1000 simulations for 2 cards
+Finished 1000 simulations for 3 cards
+Finished 1000 simulations for 4 cards
+Finished 1000 simulations for 5 cards
+Finished 1000 simulations for 6 cards
+Finished 1000 simulations for 7 cards
+Finished 1000 simulations for 8 cards
+Finished 1000 simulations for 9 cards
+Finished 1000 simulations for 10 cards
+Finished 1000 simulations for 11 cards
+Finished 1000 simulations for 12 cards
+Finished 1000 simulations for 13 cards
+Finished 1000 simulations for 14 cards
+Finished 1000 simulations for 15 cards
+Finished 1000 simulations for 16 cards
+Finished 1000 simulations for 17 cards
+Finished 1000 simulations for 18 cards
+Finished 1000 simulations for 19 cards
+Finished 1000 simulations for 20 cards
+Finished 1000 simulations for 21 cards
+Finished 1000 simulations for 22 cards
+Finished 1000 simulations for 23 cards
+Finished 1000 simulations for 24 cards
+Finished 1000 simulations for 25 cards
+Finished 1000 simulations for 26 cards
+Finished 1000 simulations for 27 cards
+Finished 1000 simulations for 28 cards
+Finished 1000 simulations for 29 cards
+Finished 1000 simulations for 30 cards
+
+
+Finished Simulations
+
+
+Results for 1 card ------>              Min: 11         Q1: 35          Median: 42              Q3: 48          Max: 64
+Results for 2 cards ------>             Min: 10         Q1: 29          Median: 36              Q3: 42          Max: 63
+Results for 3 cards ------>             Min: 10         Q1: 28          Median: 33              Q3: 39          Max: 52
+Results for 4 cards ------>             Min: 8          Q1: 26          Median: 31              Q3: 37          Max: 51
+Results for 5 cards ------>             Min: 10         Q1: 25          Median: 30              Q3: 34          Max: 49
+Results for 6 cards ------>             Min: 6          Q1: 24          Median: 29              Q3: 34          Max: 46
+Results for 7 cards ------>             Min: 7          Q1: 23          Median: 28              Q3: 33          Max: 47
+Results for 8 cards ------>             Min: 7          Q1: 22          Median: 27              Q3: 31          Max: 42
+Results for 9 cards ------>             Min: 5          Q1: 22          Median: 26              Q3: 30          Max: 45
+Results for 10 cards ------>            Min: 6          Q1: 22          Median: 26              Q3: 30          Max: 43
+Results for 11 cards ------>            Min: 5          Q1: 21          Median: 26              Q3: 30          Max: 43
+Results for 12 cards ------>            Min: 5          Q1: 21          Median: 25              Q3: 29          Max: 42
+Results for 13 cards ------>            Min: 6          Q1: 20          Median: 24              Q3: 28          Max: 41
+Results for 14 cards ------>            Min: 6          Q1: 20          Median: 25              Q3: 28          Max: 41
+Results for 15 cards ------>            Min: 6          Q1: 20          Median: 24              Q3: 28          Max: 39
+Results for 16 cards ------>            Min: 8          Q1: 20          Median: 24              Q3: 27          Max: 37
+Results for 17 cards ------>            Min: 6          Q1: 19          Median: 23              Q3: 27          Max: 38
+Results for 18 cards ------>            Min: 5          Q1: 19          Median: 23              Q3: 26          Max: 39
+Results for 19 cards ------>            Min: 7          Q1: 19          Median: 23              Q3: 26          Max: 37
+Results for 20 cards ------>            Min: 5          Q1: 19          Median: 22              Q3: 26          Max: 37
+Results for 21 cards ------>            Min: 6          Q1: 18          Median: 22              Q3: 26          Max: 35
+Results for 22 cards ------>            Min: 7          Q1: 18          Median: 22              Q3: 25          Max: 36
+Results for 23 cards ------>            Min: 4          Q1: 18          Median: 22              Q3: 25          Max: 35
+Results for 24 cards ------>            Min: 6          Q1: 18          Median: 21              Q3: 25          Max: 37
+Results for 25 cards ------>            Min: 6          Q1: 18          Median: 21              Q3: 25          Max: 35
+Results for 26 cards ------>            Min: 6          Q1: 18          Median: 21              Q3: 25          Max: 34
+Results for 27 cards ------>            Min: 5          Q1: 18          Median: 21              Q3: 24          Max: 37
+Results for 28 cards ------>            Min: 4          Q1: 18          Median: 21              Q3: 24          Max: 37
+Results for 29 cards ------>            Min: 6          Q1: 18          Median: 21              Q3: 24          Max: 35
+Results for 30 cards ------>            Min: 6          Q1: 17          Median: 21              Q3: 24          Max: 34
+```
--- main.cpp
+++ main.cpp
@@ -103,6 +103,34 @@ class Card {
             }
             std::cout << "the marked count: " << markedCt << std::endl; 
         }
+        bool isWinner(){
+            // check verticals
+            for(int i = 0; i<5; i++){
+                bool allTrue = true; 
+                for(int j=0; j<5; j++){
+                    if(!markers[i][j]) {
+                        allTrue = false; 
+                        break; 
+                    }
+                }
+                if(allTrue) return true; 
+            }
+            // check horizontals
+            for(int j=0; j<5; j++){
+                bool allTrue = true; 
+                for(int i = 0; i<5; i++){
+                    if(!markers[i][j]) {
+                        allTrue = false; 
+                        break; 
+                    }
+                }
+                if(allTrue) return true; 
+            }
+            // check diagonal
+            if(markers[0][0] && markers[1][1] && markers[2][2] && markers[3][3] && markers[4][4]) return true; 
+            if(markers[0][4] && markers[1][3] && markers[2][2] && markers[3][1] && markers[4][0]) return true; 
+            return false; 
+        }
     private: 
         std::vector<std::vector<int>> card;
         std::vector<std::vector<bool>> markers; 
@@ -119,13 +147,20 @@ class Round {
             spacesCalled = 0; 
         }
         void callNumber(){
+            spacesCalled++; 
             int numCalled = numbersCalled.back(); 
-            std::cout << "The next number is: " << numCalled << std::endl; 
+//            std::cout << "The next number is: " << numCalled << "(" << spacesCalled << ")" << std::endl; 
             for(auto & card: cards){
                 card.markSpace(numCalled); 
             }; 
             numbersCalled.pop_back(); 
-            spacesCalled++; 
+        }
+        bool isThereAWinner(){
+            bool aWinner = false; 
+            for(auto card: cards){
+                if(card.isWinner()) aWinner = true; 
+            }
+            return aWinner; 
         }
         void printCards(){
             for(auto & card: cards){
@@ -148,37 +183,70 @@ class Round {
         int spacesCalled; 
 }; 
 
+class Simulation {
+    public: 
+        Simulation(int maxNumCardsParam = 5){
+            maxNumCards = maxNumCardsParam; 
+        }
+        void runSimmy(){
+            for(int numCards = 1; numCards <= maxNumCards; numCards++){
+                std::vector<int> simulationResults; 
+                for(int i=0; i<1000; i++){
+                    Round round(numCards); 
+                    int aWinner = false; 
+                    while(!aWinner){
+                        round.callNumber(); 
+                        aWinner = round.isThereAWinner(); 
+                    };
+                    simulationResults.push_back(round.getNumSpacesCalled());  
+                }
+                std::cout << "Finished 1000 simulations for " << numCards << " card" << (numCards > 1 ? "s" : "") << std::endl; 
+                results.push_back(simulationResults); 
+            }
+            std::cout << "\n\nFinished Simulations\n\n" << std::endl; 
+        }
+        void checkMax(){
+            std::cout << "maxNumCards: " << maxNumCards << std::endl; 
+        }
+        void printResults(){
+            for(auto numCardGroup: results){
+                for(auto spacesCalled: numCardGroup){
+                    std::cout << spacesCalled << " "; 
+                }
+                std::cout << "\nend of numCardGroup\n" << std::endl; 
+            }
+        }
+        void printPercentiles(){
+            int numberOfCards = 1; 
+            for(auto numCardGroup: results){
+                std::string plural = numberOfCards > 1 ? "s" : ""; 
+                std::cout << "Results for " << numberOfCards << " card" << plural << " ------> "; 
+                sort(numCardGroup.begin(), numCardGroup.end());
+                std::cout << 
+                    "\t\tMin: " << numCardGroup[0] << 
+                    "\t\tQ1: " << numCardGroup[249] << 
+                    "\t\tMedian: " << numCardGroup[499] << 
+                    "\t\tQ3: " << numCardGroup[749] << 
+                    "\t\tMax: " << numCardGroup[999] << std::endl; 
+                numberOfCards++; 
+            }
+
+        }
+    private: 
+        std::vector<std::vector<int>> results; 
+        int maxNumCards; 
+}; 
+
 
 
 int main(){
-    std::cout << std::endl; 
-    Round round(4); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    round.callNumber(); 
-    round.printCards(); 
-    std::cout << "After " << round.getNumSpacesCalled() << " called." << std::endl; 
-    // Card myCard; 
-    // myCard.Print(); 
+    std::cout 
+        << "\n\n\n\n-----BINGO SIMULATION ANALYSIS------\n\n\n\n" 
+        << "Finding how soon games finish as more cards are added.\n\n"
+        << "Running 1000 simulations for different numbers of cards up to 30.\n\n"
+        << std::endl; 
+    Simulation simulation(30); 
+    simulation.runSimmy(); 
+    simulation.printPercentiles(); 
     return 0; 
 }
